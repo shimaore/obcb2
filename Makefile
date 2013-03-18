cde45e7b07f1701b467c4072b81a52ed5f0984a3
Stephane Alnet <stephane@shimaore.net>
2013-03-18 15:53:38 +0100

    New version, works with external pull-up.

---
+++ Makefile
@@ -0,0 +1,87 @@
+CC=avr-gcc
+LD=avr-ld
+MCU_TARGET=attiny2313a
+DUDE_TARGET=attiny2313
+AVRDUDE=avrdude
+PORT=/dev/ttyUSB0
+override CFLAGS=-Wall -g -Os -mmcu=${MCU_TARGET}
+override LDFLAGS=
+
+%.o: %.c
+	${CC} ${CFLAGS} -c $<
+
+%.elf: %.o
+	${LD} ${LDFLAGS} -Map $@.map -o $@ $< /usr/lib/avr/lib/libc.a /usr/lib/avr/lib/avr25/crttn2313a.o
+
+%.lst: %.elf
+	avr-objdump -h -S $< > $@
+
+%.lst: %.o
+	avr-objdump -h -S $< > $@
+
+%.hex: %.elf
+	avr-objcopy -j .text -j .data -O ihex $< $@
+
+program-%: %.hex
+	# Write code out
+	${AVRDUDE} -p ${DUDE_TARGET} -P ${PORT} -U flash:w:$<
+
+clean:
+	rm -f *.o *.elf *.elf.map *.lst *.hex
+
+# Project-specific
+install: program-paul_1
+
+erase:
+	${AVRDUDE} -p ${DUDE_TARGET} -P ${PORT} -e
+
+show-fuses:
+	${AVRDUDE} -p ${DUDE_TARGET} -P /dev/ttyUSB0  -U lfuse:r:/dev/stdout:h
+	${AVRDUDE} -p ${DUDE_TARGET} -P /dev/ttyUSB0  -U hfuse:r:/dev/stdout:h
+	${AVRDUDE} -p ${DUDE_TARGET} -P /dev/ttyUSB0  -U efuse:r:/dev/stdout:h
+
+set-fuse:
+	# Nothing to do here.
+
+clear-fuse:
+	# Note: bit=1 means "unprogrammed", bit=0 means "programmed"
+
+	# This is the default: internal 8MHz clock divided by 8 = 1MHz clock
+	#                                                         Divide clock by 8
+	#                                                         |Output clock on CKOUT
+	#                                                         ||SUT1
+	#                                                         |||SUT0
+	#                                                         ||||CKSEL3
+	#                                                         |||||CKSEL2
+	#                                                         ||||||CKSEL1
+	#                                                         |||||||CKSEL0
+	#                                                         ||||||||
+	${AVRDUDE} -p ${DUDE_TARGET} -P /dev/ttyUSB0 -U lfuse:w:0b01100100:m
+
+	#                                                         DebugWire
+	#                                                         |EESAVE
+	#                                                         ||SPIEN
+	#                                                         |||WDTON
+	#                                                         ||||BODLEVEL2
+	#                                                         |||||BODLEVEL1
+	#                                                         ||||||BODLEVEL0
+	#                                                         |||||||RSTDISBL
+	#                                                         ||||||||
+	${AVRDUDE} -p ${DUDE_TARGET} -P /dev/ttyUSB0 -U hfuse:w:0b11011111:m
+	# Default: BOD disabled
+
+	#                                                                SELFPRGEN
+	#                                                                |
+	${AVRDUDE} -p ${DUDE_TARGET} -P /dev/ttyUSB0 -U efuse:w:0b11111111:m
+	# Self Programming: disabled
+
+high-speed:
+	echo sck 8.68 | ${AVRDUDE} -p ${DUDE_TARGET} -P /dev/ttyUSB0 -t
+
+slow-speed:
+	echo sck 276.7 | ${AVRDUDE} -p ${DUDE_TARGET} -P /dev/ttyUSB0 -t -F
+
+voltage:
+	echo vtarg 3.0 | ${AVRDUDE} -p ${DUDE_TARGET} -P /dev/ttyUSB0 -t -F
+
+all: slow-speed clear-fuse high-speed install set-fuse
--- paul_1.c
+++ paul_1.c
@@ -1,82 +1,318 @@
-#include <avr/io.h>      // this contains all the IO port definitions
-#include <util/delay.h>
+#include <stdlib.h>
+#include <avr/io.h>     // this contains all the IO port definitions
+#include <avr/wdt.h>    // Watchdog
+#include <avr/sleep.h>  // Sleep Mode
+#include <avr/interrupt.h> // cli() etc.
 
-// This function basically wastes time
-void delay_ms( uint16_t milliseconds)
-{
-   for( ; milliseconds > 0; milliseconds--)
-   {
-      _delay_ms( 1);
-   }
+inline void status(uint8_t b) {
+  DDRB = 0xff;
+  PORTB = ~b; /* common positive */
 }
 
+#ifndef PCIE0
+#define PCIE0 PCIE
+#endif
+#ifndef PCIE2
+#define PCIE2 4
+#endif
 
-int main(void) {
+enum {
+  state_start,
+  /* In Setup mode, we let the user train us by flashing the light on and off at 0.25Hz.
+   * User must put finger when on, and take it back when off.
+   * During the first 0.5s of each 2s "on" period we do not collect data (this gives the user
+   * time to actually move their finger). We then sample for 1.5s.
+   */
+  state_setup_led_off = 0x9,
+  state_setup_led_off_waiting, /* 0.5s LED off, no data gathering */
+  state_setup_led_off_start,
+  state_setup_led_off_low,
+  state_setup_led_off_charging,
 
-  uint16_t c = 0;
+  state_setup_led_on = 0x11,
+  state_setup_led_on_waiting, /* 0.5s LED on, no data gathering ("move finger" period)*/
+  state_setup_led_on_start,
+  state_setup_led_on_low,     /* 0.03s LED on, input low = discharge */
+  state_setup_led_on_charging,     /* LED on, input Hi-Z = charge */
 
-  int on = 0;
+  /* Depending on the data gathered so far, we either
+   * - go back to state_setup_led_off (try again)
+   * - or have enough data to move to work state.
+   *
+   * In work state we sample at 20Hz and do something useful if we detect the finger
+   * is present.
+   */
+  state_work = 0x21,
+  state_work_start,        /* 0.01s Input low = discharge */
+  state_work_low,          /* input Hi-Z = charge */
+  state_work_charging
+}; uint8_t state;
 
-  DDRB = 0xFF;       // set all 8 pins on port B to outputs
-  PORTB = 0x0;       // set all pins on port B low (turn off LEDs)
+/* Keep these two in sync */
+#define intervals(t) (t/0.030)
+#define TIMEBASE WDTO_30MS
 
-  // Port D0 en Hi-Z/Output Low
-  // Autre ports Dn en Pulled-Up/Output High
-  PORTD = 0xfe;
+inline void watchdog_init() {
+  /* Enable watchdog */
+  wdt_reset();
+  /* Watchdog will trigger ~ every 30ms
+   * (based on the internal 128KHz clock). */
+  wdt_enable(TIMEBASE);
 
-  // Turn pull-ups On
-  MCUCR &= ~(1<<PUD);
+  #ifdef WDTCSR  /* 2313 */
+  /* Set WDCE (change enable), Set WDIE (interrupt) */
+  WDTCSR |= _BV(WDCE) | _BV(WDIE);
+  /* Clear WDE (no reset) */
+  WDTCSR &= ~(_BV(WDE));
+  #endif
 
-  uint16_t d = 0;
-  int led = 0;
+  #ifdef WDTCR   /* 2313A */
+  /* Set WDCE (change enable), Set WDIE (interrupt) */
+  WDTCR |= _BV(WDCE) | _BV(WDIE);
+  /* Clear WDE (no reset) */
+  WDTCR &= ~(_BV(WDE));
+  #endif
 
-  uint16_t max = 100;
-  uint16_t wait  = 0;
+  /* Reduce power, but still wake up on Watchdog, .. */
+  // set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+  set_sleep_mode(SLEEP_MODE_IDLE);
+}
 
-  while(1) {
-    if(PIND & 1) {
-      if(c > max) { // 100 works with dry fingers.
-        on = 1;
-      } else {
-        on = 0;
+inline void power_init(void) {
+  /* Disable BOD mode */
+  /* Done by fuses */
+
+  /* Disable the Analog Comparator */
+  ACSR = _BV(ACD);
+
+  /* Turn off Counter0, and USART */
+  PRR = _BV(PRTIM0) | _BV(PRUSART);
+}
+
+/* INPUT is PD6/ICPI , pin #11 on ATTiny2313A PDIP/SOIC */
+#define INPUT_PORT PORTD6
+
+inline void ports_init(void) {
+  /* Configure PORTD */
+  PORTD &= ~_BV(INPUT_PORT); /* PD6 is kept output-low/input-Hi-Z */
+  /* Note: this should already be the default. */
+}
+
+inline void set_input_low(void) {
+  /* Set to output (low) */
+  DDRD |= _BV(INPUT_PORT);
+}
+
+inline void set_input_charge(void) {
+  /* Set input Hi-Z */
+  DDRD &= ~_BV(INPUT_PORT);
+}
+
+inline void ioinit(void) {
+  /* Prevent any interrupt while we are changing things */
+  cli();
+
+  /* Initialize power-saving modes */
+  power_init();
+
+  ports_init();
+  /* Hardware initialization is done. Proceed with data initialization. */
+
+  /* Indicate the state to the watchdog handler */
+  state = state_start;
+
+  watchdog_init();
+
+  /* Let's get started (re-establish interrupts) */
+  sei();
+}
+
+/* Use port D0 (pin #2 on ATTiny2313A) as output. */
+#define LED_PIN PORTD0
+
+inline void led_off(void) {
+  DDRD |= _BV(LED_PIN);
+  PORTD &= ~_BV(LED_PIN);
+}
+inline void led_on(void) {
+  DDRD |= _BV(LED_PIN);
+  PORTD |= _BV(LED_PIN);
+}
+
+inline void start_measure(void) {
+  /* Activate noise canceler, detection on rising edge */
+  /* clkIO with /256 prescaling. */
+  TCCR1B |= _BV(ICNC1) | _BV(ICES1) | _BV(CS10);
+
+  /* Clear Input Capture Flag */
+  TIFR |= _BV(ICF1); /* "ICF1 can be cleared by writing a logic one to its bit location" */
+
+  /* Clear the counter */
+  TCNT1 = 0;
+}
+
+inline uint8_t measured(void) {
+  return TIFR & _BV(ICF1);
+}
+
+/* Watchdog timer handler */
+
+uint8_t counter = 0;
+
+inline void to(uint8_t new_state) {
+  state = new_state;
+  counter = 0;
+}
+
+uint8_t measures = 0;
+
+uint16_t led_off_max = 0;
+uint16_t led_on_min = 0xffff;
+
+const uint8_t wait_timeout = intervals(1.500);
+const uint8_t max_measures = intervals(2.500);
+
+ISR( WDT_OVERFLOW_vect, ISR_BLOCK ) {
+  counter++;
+  status(state);
+
+  switch(state) {
+    case state_start:
+      led_off_max = 0;
+      led_on_min = 0xffff;
+      led_on(); /* blink to indicate start */
+      to(state_setup_led_off);
+      return;
+
+    case state_setup_led_off:
+      measures = max_measures;
+      led_off();
+      to(state_setup_led_off_waiting);
+      return;
+
+    case state_setup_led_off_waiting:
+      if(counter >= wait_timeout) {
+        to(state_setup_led_off_start);
       }
-      // Port D0 en output 0, others Dn in input
-      DDRD = 0x01;
-      // Port D0 in input Hi-Z, others in input
-      DDRD = 0x00;
-      c = 0;
-    } else {
-      c++;
-    }
-    PORTB = (max<<1)+on;
+      return;
 
-    wait++;
-    if(wait > 10000) {
-      if(PIND & 2) {
-        max++;
+    case state_setup_led_off_start:
+      set_input_low();
+      to(state_setup_led_off_low);
+      return;
+
+    case state_setup_led_off_low:
+      start_measure();
+      set_input_charge();
+      to(state_setup_led_off_charging);
+      return;
+
+    case state_setup_led_off_charging:
+      if(measured()) {
+        uint16_t measure = ICR1;
+        // status(measure>>8);
+        if(measure > led_off_max) {
+          led_off_max = measure;
+        }
+        measures--;
+        if(measures) {
+          to(state_setup_led_off_start);
+          return;
+        }
+        /* Done measuring here */
+        to(state_setup_led_on);
       }
-      if(PIND & 4) {
-        if(max > 0){
-          max--;
+      return;
+
+    case state_setup_led_on:
+      measures = max_measures;
+      led_on();
+      to(state_setup_led_on_waiting);
+      return;
+
+    case state_setup_led_on_waiting:
+      if(counter >= wait_timeout) {
+        to(state_setup_led_on_start);
+      }
+      return;
+
+    case state_setup_led_on_start:
+      set_input_low();
+      to(state_setup_led_on_low);
+      return;
+
+    case state_setup_led_on_low:
+      start_measure();
+      set_input_charge();
+      to(state_setup_led_on_charging);
+      return;
+
+    case state_setup_led_on_charging:
+      if(measured()) {
+        uint16_t measure = ICR1;
+        // status(measure>>8);
+        if(measure < led_on_min) {
+          led_on_min = measure;
+        }
+        measures--;
+        if(measures) {
+          to(state_setup_led_on_start);
+          return;
+        }
+        /* Done measuring: Decision time! */
+        if(led_off_max < led_on_min) {
+          /* Looks good, switch to normal mode. */
+          to(state_work);
+        } else {
+          /* Setup failed, start over. */
+          to(state_start);
         }
       }
-      wait = 0;
-    }
-/*
-    if(on) {
-      d += 2;
-    } else {
-      d += 1;
-    }
-    if(d > 40) {
-      led = !led;
-      d = 0;
-    }
-    if(led) {
-        PORTB = 0xff;
-    } else {
-        PORTB = 0x00;
-    } // on
-*/
+      return;
+
+    case state_work:
+      led_off();
+      to(state_work_start);
+      return;
+
+    case state_work_start:
+      set_input_low();
+      to(state_work_low);
+      return;
+
+    case state_work_low:
+      start_measure();
+      set_input_charge();
+      to(state_work_charging);
+      return;
+
+    case state_work_charging:
+      if(measured()) {
+        uint16_t measure = ICR1;
+        if(measure <= led_off_max) {
+          led_off();
+        }
+        if(measure >= led_on_min) {
+          led_on();
+        }
+        to(state_work_start);
+      }
+      return;
+
   }
 }
+
+/* Startup */
+__attribute__((naked))    // suppress redundant SP initialization
+extern int main(void) {
+  ioinit();
+
+  while(1) {
+    sleep_mode();
+  }
+
+  return 0;
+}
+
+/* Build Chain Breakage on my machine */
+void exit(int __status) {}
